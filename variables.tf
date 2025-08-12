cfb6f350907518fcaf1a46af8050ee7c65f003b2
Shashank Venkataramana <shashankkv1403@gmail.com>
2025-08-12 13:36:45 -0700

    Running

--- variables.tf
+++ variables.tf
@@ -1,18 +1,3 @@
-variable "github_app_id" {
-  description = "GitHub App ID"
-  type        = string
-}
-
-variable "github_installation_id" {
-  description = "GitHub Installation ID"
-  type        = string
-}
-
-variable "github_app_private_key" {
-  description = "GitHub App Private Key"
-  type        = string
-}
-
 variable "github_organization" {
   description = "GitHub Organization Name"
   type        = string
