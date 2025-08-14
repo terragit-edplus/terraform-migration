5991eda2d46371afc2cbc2fe9d5f2884967e8144
svenk163 <svenk163@asu.edu>
2025-08-14 12:32:47 -0700

    Adding terraform variables

--- variables.tf
+++ variables.tf
@@ -2,3 +2,18 @@ variable "github_organization" {
   description = "GitHub Organization Name"
   type        = string
 }
+
+variable "github_app_id" {
+  description = "GitHub App ID"
+  type        = string
+}
+
+variable "github_app_installation_id" {
+  description = "GitHub App Installation ID"
+  type        = string
+}
+
+variable "app_pem_file" {
+  description = "GitHub App PEM file"
+  type        = string
+}
