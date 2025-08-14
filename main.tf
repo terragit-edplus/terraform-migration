ca89f713477610f887806a09ae896ace46938537
svenk163 <svenk163@asu.edu>
2025-08-14 13:51:28 -0700

    Adding variables

--- main.tf
+++ main.tf
@@ -1,6 +1,10 @@
 provider "github" {
     owner = var.github_organization
-    app_auth {}
+    app_auth {
+        id              = var.app_id
+        installation_id = var.app_installation_id
+        pem_file        = var.app_pem_file
+    }
 }
 
 locals {
--- variables.tf
+++ variables.tf
@@ -2,3 +2,19 @@ variable "github_organization" {
   description = "GitHub Organization Name"
   type        = string
 }
+
+variable "app_id" {
+  description = "GitHub App ID"
+  type        = string
+}
+
+variable "app_installation_id" {
+  description = "GitHub Installation ID"
+  type        = string
+}
+
+variable "app_pem_file" {
+  description = "GitHub App Private Key PEM file path"
+  type        = string
+  default     = "github_app.pem"
+}
