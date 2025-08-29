89cd9c5d0b9a840c409c9c217204cd91be088652
svenk163 <svenk163@asu.edu>
2025-08-29 15:17:36 -0700

    Adding branch protection rules

--- modules/repos/main.tf
+++ modules/repos/main.tf
@@ -111,4 +111,5 @@ resource "github_branch_protection_v3" "protection" {
     users = length(trimspace(each.value.users)) > 0 ? split(",", each.value.users) : []
     teams = length(trimspace(each.value.teams)) > 0 ? split(",", each.value.teams) : []
   }
+   depends_on = [github_branch.default, github_branch.custom ]
 }
