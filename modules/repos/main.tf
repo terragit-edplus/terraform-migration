8d13130b90758b016e7a19001b8fa008ffe1618b
svenk163 <svenk163@asu.edu>
2025-09-09 15:54:59 -0700

    Adding environments

--- modules/repos/main.tf
+++ modules/repos/main.tf
@@ -157,7 +157,7 @@ resource "github_repository_environment" "envs" {
 }
 
 resource "github_repository_environment_deployment_policy" "env_policy" {
-  for_each = local.environments
+  for_each = {for env in local.environments : "$env[0]:$env[1]" => env}
   repository = each.value[0]
   environment = each.value[1]
   tag_pattern = "${environment}"
